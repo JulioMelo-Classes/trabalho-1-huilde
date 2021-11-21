72a4032117aae45cc4f09a2263e75bae9d195576
huilde <huildevj@gmail.com>
2021-11-21 20:47:51 -0300

    corrigindo bugs

--- src/forca.cpp
+++ src/forca.cpp
@@ -3,8 +3,11 @@ using namespace std;
 #include <vector>
 #include <string>
 #include <fstream>
+#include <random>
+#include <algorithm>
+#include <iterator>
 #include "Foca.hpp"
-  /**
+/**
          * Cria a classe Forca
          * O construtor pode ou não validar os arquivos de entrada, no entanto, mesmo com 
          * os arquivos inválidos o objeto deve ser construído. Para o caso de arquivos de palavras
@@ -13,63 +16,120 @@ using namespace std;
          * @param scores o nome do arquivo que contém os scores
          * @see eh_valido
          */
-        Forca::Forca(std::string palavras, std::string scores ){
-            m_arquivo_palavras = palavras;
-            m_arquivo_scores = scores;
-          
-        };
-       
- 
-        /**
+Forca::Forca(std::string palavras, std::string scores)
+{
+  m_arquivo_palavras = palavras;
+  m_arquivo_scores = scores;
+};
+
+/**
          * Valida os arquivos de entrada de acordo com as especificações.
          * Ao validar os arquivos, no caso de arquivos inválidos, este método deve retornar a 
          * razão correspondente de acordo com as especificações.
          * @return {T,""} se os arquivos estiverem válidos, {F,"razão"} caso contrário.
          */
-        std::pair<bool, std::string> eh_valido();
- 
-        /**
+std::pair<bool, std::string> eh_valido();
+
+/**
          * Carrega os arquivos de scores e palavras preenchendo **ao menos** a estrutura m_palavras
          */
-        void  Forca::carrega_arquivos(){
-        std::fstream arquivoPalavras;
-        arquivoPalavras.open(m_arquivo_palavras, ios::in);
-        int frequencia;
-        std::string palavra;
-        while (!arquivoPalavras.eof())
-        {
-          arquivoPalavras >> frequencia >> palavra;
-          m_palavras.push_back(make_pair(palavra, frequencia));
-        }
-        }
- 
-        /**
+void Forca::carrega_arquivos()
+{
+  std::fstream arquivoPalavras;
+  arquivoPalavras.open(m_arquivo_palavras, ios::in);
+  int frequencia;
+  std::string palavra;
+  while (!arquivoPalavras.eof())
+  {
+    arquivoPalavras >> frequencia >> palavra;
+    m_palavras.push_back(make_pair(palavra, frequencia));
+  }
+  m_palavras.pop_back();
+  int media = 0;
+  std::random_device rd;
+  std::mt19937 g(rd());
+
+  // Embaralha o vetor `v` usando o motor de números aleatórios `g`.
+  std::shuffle(m_palavras.begin(), m_palavras.end(), g);
+  for (int i = 0; i < m_palavras.size(); i++)
+  {
+    media += m_palavras[i].second;
+  }
+  media = media / (m_palavras.size());
+  for (int i = 0; i < m_palavras.size(); i++)
+  {
+    if (m_palavras[i].second > media)
+    {
+      MaiorMedia.push_back(m_palavras[i]);
+    }
+    else
+    {
+      MenorMedia.push_back(m_palavras[i]);
+    }
+  }
+}
+
+/**
          * Modifica a dificuldade do jogo.
          * De acordo com a dificuldade configurada, o método proxima_palavra retornará palavras
          * diferentes.
          * @param d a dificuldade desejada
          * @see proxima_palavra
          */
-         void Forca::set_dificuldade(Dificuldade i){
-           d = i;
-         }
- 
-        /**
+void Forca::set_dificuldade(Dificuldade i)
+{
+  d = i;
+}
+
+/**
          * Retorna a próxima palavra de acordo com a dificuldade atual.
          * Este método deve atualizar o atributo m_palavra_atual e retornar a palavra um texto no formato
          * "_ _ _ _ ... _" dependendo do tamanho de m_palavra_atual. O método também deve sortear as 
          * letras que devem aparecer dependendo do nível de dificuldade.
          * @return a próxima palavra do conjunto de palavras disponível de acordo com a dificuldade atual.
          */
-        std::string proxima_palavra();
- 
-        /**
+std::string Forca::proxima_palavra()
+{
+  //retornando a palavra correta com base na dificuldade do jogo
+  std::string retorno;
+  if (d == FACIL)
+  {m_palavra_atual = MaiorMedia[0].first;
+  for (int i = 0 ; i < MaiorMedia[0].first.size(); i++)
+  {
+   retorno.push_back('_');
+  }
+  MaiorMedia.erase(MaiorMedia.begin());
+
+  }
+else if(d == MEDIO)
+{m_palavra_atual = MaiorMedia[0].first;
+  for (int i = 0 ; i < MaiorMedia[0].first.size(); i++)
+  {
+    retorno.push_back('_');
+  }
+   MaiorMedia.erase(MaiorMedia.begin());
+}
+  
+  else
+  {m_palavra_atual = MenorMedia[0].first;
+  for (int i = 0 ; i < MenorMedia[0].first.size(); i++)
+  {
+   retorno.push_back('_');
+  }
+  MenorMedia.erase(MenorMedia.begin());
+  }
+m_palavraSecreta = retorno;
+return retorno;}
+
+/**
          * Retorna a palavra atual que está sendo jogada.
          * @return o valor do atributo m_palavra_atual.
          */
-        std::string get_palavra_atual();
- 
-        /**
+std::string Forca::get_palavra_atual(){
+  return m_palavra_atual;
+}
+
+/**
          * Testa se uma letra pertence á palavra atual, retorna T se a letra pertence.
          * Este método testa se uma letra pertence à palavra atual e retorna True caso pertença. 
          * Caso contrário, o método deve atualizar o atributo m_tentativas_restantes, decrementando 
@@ -77,25 +137,102 @@ using namespace std;
          * @param palpite uma letra, que deve ser testada se pertence à palavra.
          * @return T se a palpite pertence à palavra, F caso contrário.
          */
-        bool palpite(std::string palpite);
+bool Forca::palpite(char palpite){
+  bool resultado = false;
+  for (int i = 0; i <m_palavra_atual.size(); i++)
+  {if(toupper(palpite) == m_palavra_atual[i]){
+    m_palavraSecreta[i] = m_palavra_atual[i];
+    resultado = true;
+    m_pontos+=1;
+  }
+  }
+  if(resultado == false){
+    m_tentativas_restantes =m_tentativas_restantes -1;
+    m_pontos-=1;
+  }
  
-        /**
+return resultado;}
+
+/**
          * Em caso de Game Over este método deve retornar T.
          * @return T caso o m_tentativas_restantes do jogo esteja igual a 0, F caso contrário.
          */
-        bool game_over();
- 
-        /**
+bool Forca::game_over(){
+  if(m_tentativas_restantes>0){
+    return false;
+  }
+  else{
+    return true;
+  }
+}
+
+/**
          * Atualiza o número de tentativas restantes.
          * Este método é útil no caso do jogador escolher continuar o jogo.
          * @param tentativas atualiza o valor da variável m_tentativas_restantes.
          */
-        void set_tentativas_restantes(int tentativas);
- 
-        /**
+void Forca::set_tentativas_restantes(int tentativas){
+  m_tentativas_restantes = 6;
+}
+
+/**
          * Retorna a quantidade de tentativas restantes.
          * @return a quantidade de tentativas restantes.
          */
-        int get_tentativas_restantes();
- 
+int Forca::get_tentativas_restantes(){
+  return m_tentativas_restantes;
+}
+std::string Forca::GetpalavraSecreta(){
+return m_palavraSecreta;
+}
+void Forca::SetpalavraSecreta(std::string palavra){
+  m_palavraSecreta = palavra;
+}
+ int Forca::getpontos(){
+   return m_pontos;
+ }
+bool Forca::ContaPalavras(){
+if(d == MEDIO||d==FACIL && MaiorMedia.size()>0){
+return true;
+}
+else if(d==DIFICIL&& MenorMedia.size()>0){
+  return true;
+}
+else{
+  return false;
+}
+}
+void Forca::imprimeForca(){
+  switch(m_tentativas_restantes)
+  {
+  case 6:
+    cout<<endl<<endl<<endl<<endl<<endl;
+    break;
+  case 5:
+    cout<<"O"<<endl<<endl<<endl<<endl;
+    break;
+    case 4:
+    cout<<' '<<"O"<<endl;
+    cout<<" "<<"|"<<endl<<endl<<endl;
+    break;
+    case 3:
+    cout<<" O"<<endl;
+     cout<<"/|"<<endl<<endl<<endl;
+    break;
+    case 2:
+    cout<<" O"<<endl;
+     cout<<"/|\\"<<endl<<endl<<endl;
+    break;
+    case 1:
+    cout<<" O"<<endl;
+    cout<<"/|\\"<<endl;
+    cout<<"/"<<endl<<endl;;
+    break;
+    case 0:
+    cout<<" O"<<endl;
+    cout<<"/|\\"<<endl;
+    cout<<"/ \\"<<endl<<endl;
+    break;
 
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,18 @@
 #include <vector>
 #include <string>
 #include <fstream>
-using namespace std;
+#include <random>
+#include <algorithm>
+#include <iterator>
 
+using namespace std;
+//token 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
 int main(int argc, char *argv[])
 {
-
+  std::fstream arquivoPontos;
   std::string arquivoCorpus = argv[1];
   std::string arquivoScores = argv[2];
-
-  
+  char letra;
   // ../include/palavras.txt
   Forca Jogo = Forca(arquivoCorpus, arquivoScores);
   cout << ">>> Lendo arquivos de palavras"
@@ -21,48 +24,139 @@ int main(int argc, char *argv[])
        << "por favor aguarde.." << endl;
   //adicionar a validação dos arquivos.
   int escolha;
-  cout << "--------------------------------------------------------------------" << endl;
-  cout << "Bem vindo ao Jogo Forca! Por favor escolha uma das opções" << endl;
-  cout << "1 - Iniciar Jogo" << endl;
-  cout << "2 - Ver scores anteriores" << endl;
-  cout<<"sua escolha:";
-  cin >> escolha;
-  if (escolha == 1)
-  {
-    Jogo.carrega_arquivos();      
-    cout << "Vamos iniciar o jogo! Por favor escolha o nível de dificuldade" << endl;
-    cout<<"1 - Fácil"<<endl;
-    cout<<"2 - Médio"<<endl;
-    cout<<"3 - Difícil"<<endl;
-    cout<<"sua escolha: ";
-    cin>>escolha;
-    if(escolha==1){
-      cout<<"fácil";
-    }
-    else if(escolha==2){
-      cout<<"médio";
-    }
-    else if(escolha==3){
-      cout<<"Difícil";
-    }
-    else{
-      cout<<"Escolha uma opção válida"<<endl;
-    }
-  }
-  else if (escolha == 2)
-  {
-     std::fstream arquivoPontos;
-  arquivoPontos.open(arquivoScores, ios::in);
-  std::string pontos;
-  while (!arquivoPontos.eof())
-  {getline(arquivoPontos,pontos);
-  cout<<pontos<<endl;
+  //while  englobando todo o escopo da função;
 
-  }
+  while (true)
+  {
+    int pontos = 0;
+    cout << "--------------------------------------------------------------------" << endl;
+    cout << "Bem vindo ao Jogo Forca! Por favor escolha uma das opções" << endl;
+    cout << "1 - Iniciar Jogo" << endl;
+    cout << "2 - Ver scores anteriores" << endl;
+    cout << "sua escolha:";
+    cin >> escolha;
+    if (escolha == 1)
+    {
+      std::vector<std::string> Acertadas = {};
+
+      Jogo.carrega_arquivos();
+      cout << "Vamos iniciar o jogo! Por favor escolha o nível de dificuldade" << endl
+           << "1 - Fácil" << endl
+           << "2 - Médio" << endl
+           << "3 - Difícil" << endl;
+      cout << "Sua escolha: ";
+      cin >> escolha;
+      if (escolha == 1)
+      {
+        Forca::Dificuldade i = Forca::Dificuldade::FACIL;
+        Jogo.set_dificuldade(i);
+        cout<<"Iniciando o Jogo no nível Fácil, será que você conhece essa palavra?"<<endl;
+      }
+      else if (escolha == 2)
+      {
+        Forca::Dificuldade i = Forca::Dificuldade::MEDIO;
+        Jogo.set_dificuldade(i);
+        cout<<"Iniciando o Jogo no nível Médio, será que você conhece essa palavra?"<<endl;
+      }
+      else if (escolha == 3)
+      {
+        Forca::Dificuldade i = Forca::Dificuldade::DIFICIL;
+        Jogo.set_dificuldade(i);
+        cout<<"Iniciando o Jogo no nível Difícil, será que você conhece essa palavra?"<<endl;
+      }
+      while (Jogo.ContaPalavras())
+      {
+        std::string secreta = Jogo.proxima_palavra();
+        while (!Jogo.game_over() && Jogo.GetpalavraSecreta() != Jogo.get_palavra_atual())
+        {
+          Jogo.imprimeForca();
+
+          cout << endl;
+          for (size_t i = 0; i < Jogo.get_palavra_atual().size(); i++)
+          {
+            cout << Jogo.GetpalavraSecreta()[i] << " ";
+          }
+          cout << "pontos: " << Jogo.getpontos() << endl;
+          cout << "palpite: ";
+          cin >> letra;
+          std::string teste = Jogo.get_palavra_atual();
+          if (Jogo.palpite(letra))
+          {
+            cout << "Muito bem! A palavra contém a letra " << letra << "!" << endl;
+            Jogo.imprimeForca();
+            cout << "pontos: " << Jogo.getpontos() << endl;
+          }
+          else
+          {
+            cout << "Meh, não achei a letra " << letra << " :<" << endl;
+          }
+        }
+
+        if (Jogo.GetpalavraSecreta() == Jogo.get_palavra_atual())
+        {
+          cout << "Parabéns, você acertou a palavra" << endl;
+          Acertadas.push_back(Jogo.GetpalavraSecreta());
+          Jogo.imprimeForca();
+          cout << "pontos: " << Jogo.getpontos() << endl;
+          cout << "Deseja continuar jogando?" << endl;
+          cout << "1 - Continuar" << endl
+               << "2 -Parar" << endl;
+          int decisao;
+          cout << "Escolha: ";
+          cin >> decisao;
+          if (decisao == 2)
+          {
+            break;
+          }
+        }
+        else
+        {
+          cout << "Vixe, Fim de Jogo!" << endl;
+          Jogo.imprimeForca();
+          cout << "pontos: " << Jogo.getpontos() << endl;
+          cout << "O jogo acabou a palavra era " << Jogo.get_palavra_atual() << endl;
+        }
+
+        Jogo.set_tentativas_restantes(6);
+      }
+      if(!Jogo.ContaPalavras()){
+        cout<<"O Jogo foi encerrado pois você acertou todas as palavras disponivéis"<<endl;
+      }
+      cout<<"Digite seu nome para podermos Guardar a sua pontuaçao"<<endl;
+      std::string nomeUsuario; 
+      cin.ignore();
+      getline(cin,nomeUsuario,'\n');
+      arquivoPontos.open(arquivoScores, ios::app);
+      arquivoPontos<<"Dificil; "<<nomeUsuario<<"; ";
+      for(int i =0;i< Acertadas.size();i++){
+        arquivoPontos<<Acertadas[i]<<",";
+      }
+       arquivoPontos<<"; "<<Jogo.getpontos();
+       
+
+    }
+    else if (escolha == 2)
+    {
+      arquivoPontos.open(arquivoScores, ios::in);
+      std::string pontos;
+      while (!arquivoPontos.eof())
+      {
+        getline(arquivoPontos, pontos);
+        cout << pontos << endl;
+      }
     
+         cout<<"Deseja Jogar?"<<endl
+              <<"1 - Sim"<<endl
+              <<"2 - Não"<<endl;
+               int decisao;
+          cout << "Escolha: ";
+          cin >> decisao;
+          if (decisao == 2)
+          {
+            break;
+          }
+
+    }
   }
-else{
-  cout<<"Escolha uma opção válida"<<endl;
-}
   return 0;
 }
